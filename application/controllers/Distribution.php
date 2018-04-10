43a67930ab6a7672c91b74550326fcb2c37f1a76
Tri NGO <t.ngo@santiane.fr>
2018-04-10 16:02:09 +0200

    distribution

--- application/controllers/Distribution.php
+++ application/controllers/Distribution.php
@@ -11,15 +11,29 @@ class Distribution extends CI_Controller {
 
     public function index($player = array('pierre', 'paul', 'jaques'))
     {
-        $nbPlayer = count($player);
+        $this->load->helper('MY_HelP');
 
-        for ($i = 0; $i <= $nbPlayer; $i++)
+        $nbPlayer = count($player);
+        $player[$nbPlayer] = 'center';
+        $player[$nbPlayer + 1] = 'out';
+
+        for ($i = 0; $i <= $nbPlayer + 1; $i++)
             $hand[$i] = '0000000000000';
 
         for ($i = 0; $i < 52; $i++)
             $sort[$i] = $i;
 
-        for ($i = 0; $i < 52; $i++) {
+        $card = array_rand($sort);
+
+        $color = $card % 4;
+        $rank = ($card - $color) / 4;
+
+        $newColor = dechex(hexdec($hand[$nbPlayer + 1][$rank]) + pow(2, $color));
+        $hand[$nbPlayer + 1][$rank] = $newColor;
+
+        unset($sort[$card]);
+
+        for ($i = 0; 0 < count($sort); $i++) {
             $card = array_rand($sort);
 
             $color = $card % 4;
@@ -30,29 +44,9 @@ class Distribution extends CI_Controller {
 
             unset($sort[$card]);
         }
-        print_r($hand);
+
+//        transfert ($hand, 0, $nbPlayer, $color, $nb);
+
+        affAllCards ($hand, $player);
     }
-
-
-
-
-
-//    for ($j = 0; $j < $nbPlayer; $j++) {
-//
-//        echo '<br>'.$player[$j];
-//
-//        for ($i = 0; isset($hand[$j][$i]); $i++) {
-//
-//            $bin = hexdec($hand[$j][$i]);
-//            if (1 <= $bin % 2)
-//                echo('<br>' . $i . ' ' . 'pique');
-//            if (2 <= $bin % 4)
-//                echo('<br>' . $i . ' ' . 'trefle');
-//            if (4 <= $bin % 8)
-//                echo('<br>' . $i . ' ' . 'carreau');
-//            if (8 <= $bin % 16)
-//                echo('<br>' . $i . ' ' . 'coeur');
-//        }
-    //echo '<br>';
-
 }
